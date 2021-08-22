13e0393e5f835d115b6747590a60084cd2ed9ec8
IvanSergeyev <ivan.sergeyev29@yandex.ru>
2021-08-22 17:58:56 +0600

    preparing

---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
+const cors = require('cors');
+require('dotenv').config();
+
+const { createUser, login } = require('./controllers/users');
+const userRouter = require('./routes/users');
+const cardRouter = require('./routes/cards');
+
+const auth = require('./middlewares/auth');
+const { validateUser, validateLogin } = require('./middlewares/validation');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const NotFoundError = require('./errors/NotFoundError');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+app.post('/signup', validateUser, createUser);
+app.post('/signin', validateLogin, login);
+
+app.use(auth);
+
+app.use(userRouter);
+app.use(cardRouter);
+
+app.use('*', () => {
+  throw new NotFoundError('Запрашиваемый ресурс не найден');
+});
+
+app.use(errorLogger);
+
+app.use(errors());
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+  next();
+});
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
