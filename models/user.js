f4300a6f02cd7ad4327f3c2e1cb99b766ef4f764
IvanSergeyev <ivan.sergeyev29@yandex.ru>
2021-08-26 12:37:50 +0600

    solve problem with bycrypt

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const AuthError = require('../errors/AuthError');
 
